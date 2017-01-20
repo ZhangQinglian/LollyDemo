3d5630e973ab615695afb1cee911842472182c91
Scott ZHANG <zqlxtt@live.com>
2017-01-20 11:19:12 +0800

    fix some bug

--- app/src/main/java/com/zqlite/android/lollydemo/MainActivity.java
+++ app/src/main/java/com/zqlite/android/lollydemo/MainActivity.java
@@ -1,83 +1,82 @@
 package com.zqlite.android.lollydemo;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 
+import com.zqlite.android.logly.Logly;
 import com.zqlite.android.lolly.Lolly;
 import com.zqlite.android.lollydemo.artphelper.ARTPHelper;
+import com.zqlite.android.onepiece.OnePiece;
+import com.zqlite.android.onepiece.ViewID;
 
 public class MainActivity extends AppCompatActivity {
 
+    @ViewID(R.id.show_window)
     private Button mShowWindowBtn ;
+    @ViewID(R.id.hide_window)
     private Button mHideWindowBtn;
-    private Button mCollectLog;
+    @ViewID(R.id.save_log)
     private Button mSaveLog;
     final ARTPHelper artpHelper = new ARTPHelper(true);
 
-
-    public static final int OVERLAY_PERMISSION_REQ_CODE  = 370 ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        OnePiece.own().initViews(this);
 
         artpHelper.writeExternalStorage();
         artpHelper.requestPermissions(this);
 
-        mShowWindowBtn = (Button) findViewById(R.id.show_window);
         mShowWindowBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
                     if (Settings.canDrawOverlays(MainActivity.this)) {
-                        Lolly.try2StartLolly(MainActivity.this, new String[]{"scott"});
+                        Lolly.showLolly(MainActivity.this, new String[]{"scott"});
                     }else{
                         artpHelper.tryToDropZone(MainActivity.this);
                     }
                 }else{
-                    Lolly.try2StartLolly(MainActivity.this, new String[]{"scott"});
+                    Lolly.showLolly(MainActivity.this, new String[]{"scott"});
                 }
 
             }
         });
 
-        mHideWindowBtn = (Button) findViewById(R.id.hide_window);
         mHideWindowBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Lolly.stopLolly(MainActivity.this);
+                Lolly.hideLolly(MainActivity.this);
             }
         });
 
-        mCollectLog = (Button) findViewById(R.id.collect_log);
-        mCollectLog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Lolly.justStartCollectLog(MainActivity.this);
-            }
-        });
-
-        mSaveLog = (Button) findViewById(R.id.save_log);
         mSaveLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Lolly.stopLollyAndSaveLog(MainActivity.this);
+                Lolly.saveLog(MainActivity.this);
             }
         });
 
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Logly.i("this is Lolly");
+                Logly.d("this is Lolly");
+                Logly.v("this is Lolly");
+                Logly.e(new Logly.Tag(Logly.FLAG_THREAD_NAME,"scott",Logly.ERROR),"this is Lolly");
+                Logly.w("this is Lolly");
+                handler.postDelayed(this,2000);
+            }
+        },2000);
 
     }
 
@@ -85,4 +84,5 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         artpHelper.onRequestPermissionsResult(requestCode,permissions,grantResults);
     }
+
 }
